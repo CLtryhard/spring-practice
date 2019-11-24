31917974e8d8508e40298a1453afb1a34087e495
Lance <cltryhard@163.com>
2019-11-24 23:41:49 +0800

    test delete ComponentScan.

--- chapter_02/src/main/java/soundsystem/config/CDPlayerConfig.java
+++ chapter_02/src/main/java/soundsystem/config/CDPlayerConfig.java
@@ -1,16 +1,18 @@
 package soundsystem.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
 /**
  * //@ComponentScan 不指定基础包时,则只扫描本类所在包及其子包
  * //@ComponentScan(basePackageClasses = SgePeppers.class)
  */
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author Lance
  * @version 1.0.0
  * @date 2019/11/10
+ * 直接去除包扫描注解后,会抛出{@link org.springframework.beans.factory.UnsatisfiedDependencyException}
  */
 @Configuration
 @ComponentScan(basePackages = "soundsystem")
--- chapter_02/src/test/java/soundsystem/CDPlayerTest.java
+++ chapter_02/src/test/java/soundsystem/CDPlayerTest.java
@@ -43,5 +43,4 @@ public class CDPlayerTest {
                 outRule.getLog());
     }
 
-
 }
